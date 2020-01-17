0e3a84e60ce6e7434ae345104744ad45c06a0287
HarryVerhoef <h.verhoef@warwick.ac.uk>
2020-01-17 20:36:36 +0000

    Voting now displayed in UI

--- juke/HostLobby.js
+++ juke/HostLobby.js
@@ -170,13 +170,17 @@ export default class HostLobby extends Component {
             // Received a message from lambda, probably a vote message
             var votes = JSON.parse(evt.data);
             // this.state.recommendations.forEach((item) => {})
-            Alert.alert(votes);
-            newVotes = this.state.votes;
-            // votes.forEach((item) => {
-            //     newVotes[item.track_id.S] = item.vote_no.N;
-            // });
+            // Alert.alert(votes);
+            // newVotes = this.state.votes;
+            let newVotes = Object.assign({}, this.state.votes);
+            votes.forEach((item) => {
+                newVotes[item.track_id.S] = item.vote_no.N;
+            });
+
+            Alert.alert(newVotes);
 
             this.setState({votes: newVotes});
+
         };
 
         this.ws.onclose = () => {
